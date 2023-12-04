1d53fcd4e91862eec9075df2667864e4b53829a6
Juho Pennanen <juho.pennanen@ecomond.com>
2023-12-04 09:03:45 +0200

    🚀Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onload = function(){
+    console.log("Script loaded")
+    init();
+}
+const init = function(){
+    console.log("Init")
+    
+}
+const gameLoop = function(){
+    
+}
+setInterval(gameLoop,1000/15)
